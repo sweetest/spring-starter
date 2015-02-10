b67ddca9d8f221de3e47559bac6a218945c19a62
SEUNGJIN LEE <sweetest.sj@navercorp.com>
2015-02-10 20:27:33 +0900

    first commit

---
+++ src/main/java/example/HelloWorldController.java
@@ -0,0 +1,25 @@
+package example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by Adrian Seungjin Lee on 2015. 2. 10.
+ * Contacts : sweetest.sj@navercorp.com
+ */
+@Controller
+public class HelloWorldController {
+    @RequestMapping("/")
+    public void test(){
+        System.out.println("test");
+    }
+    @RequestMapping("/hello")
+    public ModelAndView helloWorld(){
+        System.out.println("hey");
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("hello");
+        mv.addObject("message","Hello World");
+        return mv;
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/hello.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: naver
+  Date: 2015. 2. 10.
+  Time: 오전 9:44
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+hello<br>
+${hello.message}
+</body>
+</html>
